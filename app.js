99cb87b8cdc71ee69f453c73d120f0cf9aaea197
Fabricio Godoy <fabriciogodoy95@hotmail.com>
2021-11-12 19:25:15 -0300

    agregando expenciones npm y express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`escuchando en http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+});
