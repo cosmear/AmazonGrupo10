64fcd7416577e3cf85742dbad8c9cd4402e8a511
Fabricio Godoy <fabriciogodoy95@hotmail.com>
2021-11-23 12:52:43 -0300

    agregando modificaciones

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.html"));
+});
+
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/productCart.html"));
+});
