c50e78f7bcb8ec8470b264894468e21d021f48c9
Fabricio Godoy <fabriciogodoy95@hotmail.com>
2021-11-24 23:35:56 -0300

    modificando el puerto para heroku

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
 app.use(express.static("public"));
 
 app.listen(process.env.PORT || 3000, ()=>{
